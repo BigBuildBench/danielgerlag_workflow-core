b4fc668986695a7fb4c9b68307aa2e2427ec038f
Linghao Zhang <starrywhuzhang@gmail.com>
2024-11-20 19:23:52 +0800

    init instance

---
+++ src/WorkflowCore.DSL/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using WorkflowCore.Interface;
+using WorkflowCore.Services.DefinitionStorage;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddWorkflowDSL(this IServiceCollection services)
+        {
+            services.AddTransient<ITypeResolver, TypeResolver>();
+            services.AddTransient<IDefinitionLoader, DefinitionLoader>();
+            return services;
+        }
+    }
+}
+
